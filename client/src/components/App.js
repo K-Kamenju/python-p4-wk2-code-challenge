60d00f6f79162dfa478a76ae28cc3852e3b8fc4c
Caleb <kamenjuhkiiru@gmail.com>
2024-01-22 19:56:09 +0300

    Restaurant, Home, Header components created

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,34 @@
-
+import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import Restaurant from "./Restaurant";
+import Home from "./Home";
+import RestaurantPizzaForm from "./RestaurantPizzaForm";
+import Pizza from "./Pizza";
+import PizzaEditForm from "./PizzaEditForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/restaurant_pizzas/new">
+            <HeroPowerForm />
+          </Route>
+          <Route exact path="/pizzas/:id/edit">
+            <PowerEditForm />
+          </Route>
+          <Route exact path="/pizzas/:id">
+            <Power />
+          </Route>
+          <Route exact path="/restaurants/:id">
+            <Hero />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ client/src/components/Restaurant.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+function Restaurant() {
+  const [{ data: restaurant, error, status }, setRestaurant] = useState({
+    data: null,
+    error: null,
+    status: "pending",
+  });
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`/restaurants/${id}`)
+      .then((r) => r.json())
+      .then((restaurant) =>
+        setRestaurant({ data: restaurant, error: null, status: "resolved" })
+      )
+      .catch((err) =>
+        setRestaurant({ data: null, error: err.error, status: "rejected" })
+      );
+  }, [id]);
+
+  if (status === "pending") return <h1>Loading...</h1>;
+  if (status === "rejected") return <h1>Error: {error.error}</h1>;
+
+  return (
+    <section>
+      <h2>{restaurant.name}</h2>
+      <h3>Pizzas:</h3>
+      <ul>
+        {restaurant.restaurant_pizzas.map((pizza) => (
+          <li key={pizza.id}>
+            <Link to={`/pizzas/${pizza.pizza.id}`}>{pizza.pizza.name}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/restaurant_pizzas/new">Add Pizza to Restaurant</Link>
+    </section>
+  );
+}
+
+export default Restaurant;
