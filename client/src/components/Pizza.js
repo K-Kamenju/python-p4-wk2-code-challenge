1d80956ed9e54d1fdba87bcf0e1f46d387bceaaa
Caleb <kamenjuhkiiru@gmail.com>
2024-01-22 19:57:09 +0300

    All components created

---
+++ client/src/components/Pizza.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+function Pizza() {
+  const [{ data: pizza, error, status }, setPizza] = useState({
+    data: null,
+    error: null,
+    status: "pending",
+  });
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`/pizzas/${id}`)
+      .then((r) => r.json())
+      .then((pizza) =>
+        setPizza({ data: pizza, error: null, status: "resolved" })
+      )
+      .catch((err) =>
+        setPizza({ data: null, error: err.error, status: "rejected" })
+      );
+  }, [id]);
+
+  if (status === "pending") return <h1>Loading...</h1>;
+  if (status === "rejected") return <h1>Error: {error.error}</h1>;
+
+  return (
+    <section>
+      <h2>{pizza.name}</h2>
+      <p>{pizza.ingredients}</p>
+      <p>
+        <Link to="/restaurant_pizzas/new">Add Restaurant pizza</Link>
+      </p>
+      <p>
+        <Link to={`/pizzas/${pizza.id}/edit`}>Edit pizza Ingredients</Link>
+      </p>
+    </section>
+  );
+}
+
+export default Pizza;
---
+++ client/src/components/PizzaEditForm.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router";
+
+function PizzaEditForm() {
+  const [{ data: pizza, errors, status }, setPizza] = useState({
+    data: null,
+    errors: [],
+    status: "pending",
+  });
+  const [ingredients, setingredients] = useState("");
+  const history = useHistory();
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`/pizzas/${id}`)
+      .then((r) => r.json())
+      .then((pizza) => {
+        setPizza({ data: pizza, errors: [], status: "resolved" });
+        setingredients(pizza.ingredients);
+      })
+      .catch((err) =>
+        setPizza({ data: null, errors: [err.error], status: "rejected" })
+      );
+  }, [id]);
+
+  if (status === "pending") return <h1>Loading...</h1>;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(`/pizzas/${pizza.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ingredients,
+      }),
+    })
+      .then((r) => r.json())
+      .then((updatedPizza) => {
+        setPizza({ data: updatedPizza, errors: [], status: "resolved" });
+        history.push(`/pizzas/${updatedPizza.id}`);
+      })
+      .catch((err) =>
+        setPizza({ data: pizza, errors: err.errors, status: "rejected" })
+      );
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Editing {pizza.name}</h2>
+      <label htmlFor="ingredients">Ingredients:</label>
+      <textarea
+        id="ingredients"
+        name="ingredients"
+        rows="4"
+        value={ingredients}
+        onChange={(e) => setingredients(e.target.value)}
+      />
+      {errors.length > 0
+        ? errors.map((err) => (
+            <p key={err} style={{ color: "red" }}>
+              {err}
+            </p>
+          ))
+        : null}
+      <button type="submit">Update pizza</button>
+    </form>
+  );
+}
+
+export default PizzaEditForm;
